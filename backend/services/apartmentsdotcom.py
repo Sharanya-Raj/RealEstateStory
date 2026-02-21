05e981827c3b64a9bf0105016c12cc7803d3303c
OmRanALot <omranamile@gmail.com>
2026-02-21 14:47:22 -0500

    what i have

---
+++ backend/services/apartmentsdotcom.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import path
+import sys
+import os
+import geolocate
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, ".."))
+
+from model import Apartment
+
+
+def get_apartmentsdotcom(location):
+    location = location.replace(" ", "-").lower()
+    results = geolocate.get_coordinates(location)
+
+    url = f"https://www.apartments.com/{results['name'].replace(' ', '-')}/"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    apartments = []
+    
+    for card in soup.select(".property"):
+        try:
+            name = card.select_one(".property-title").text.strip()
+            price = card.select_one(".property-pricing").text.strip()
+            
+            apartments.append(
+                Apartment(
+                    name=name,
+                    address=results['display_name'],
+                    price=price if price else "N/A",  # demo fallback
+                    bedrooms=2,
+                    latitude=results['latitude'],
+                    longitude=results['longitude'],
+                    source="Apartments.com",
+                    score=0
+                )
+            )
+        except:
+            continue
+
+    return apartments
+
+if __name__ == "__main__":
+    print(get_apartmentsdotcom("Rutgers University"))
---
+++ backend/services/geolocate.py
@@ -0,0 +1,29 @@
+import requests
+import regex as re
+
+def get_coordinates(place):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {"q": place, "format": "json"}
+    
+    response = requests.get(url, params=params, headers={"User-Agent": "apt-app"})
+    data = response.json()
+    
+    print(data) #debugging
+
+    if not data:
+        return 0, 0, ""  # Default to (0, 0) if no results found
+    
+    return_obj = {
+        "name": place,
+        "latitude": float(data[0]["lat"]),
+        "longitude": float(data[0]["lon"]),
+        "state": str(data[0].get("address", {}).get("state", "")),
+        "zipcode": re.search(r"\b\d{5}\b", data[0]["display_name"]).group() if re.search(r"\b\d{5}\b", data[0]["display_name"]) else "",
+        "display_name": str(data[0]["display_name"])
+    }
+
+    return return_obj
+if __name__ == "__main__":
+    result = get_coordinates("Rutgers University")
+    print("\n\n")
+    # print(f"Latitude: {result['latitude']}, Longitude: {result['longitude']}, State: {result['state']}, Zipcode: {result['zipcode']}, Name: {result['display_name']}")
